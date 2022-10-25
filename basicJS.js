6018882d030adf075a03f871419844883cf5b3d9
Alexandr Pirogov <otbitij@mail.ru>
2022-10-25 19:07:45 +0300

    equals

--- basicJS.js
+++ basicJS.js
@@ -1,20 +1,9 @@
-// Declare the myGlobal variable below this line
-const myGlobal = 10
-
-function fun1() {
-  // Assign 5 to oopsGlobal Here
-oopsGlobal = 5
-}
-
-// Only change code above this line
-
-function fun2() {
-  var output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
+// Setup
+function testNotEqual(val) {
+    if (val != 99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
   }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-}
+  
+  testNotEqual(10);
