b3940ac6a5e817ffa7fecf042bdc2a9e7a401f02
Alexandr Pirogov <otbitij@mail.ru>
2023-02-07 11:09:32 +0300

    безуспешная первая задача

---
+++ hw.js
@@ -0,0 +1,18 @@
+// Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом. Например:
+
+// ucFirst("вася") == "Вася"
+function ucFirst(str) {
+    for (let i = 0; i < str.lenght; i++ ) {
+        if (i = 0) {
+            str[0].toUpperCase
+        }
+        str[i] += str[i++]
+    }
+    return str
+}
+console.log(ucFirst("линия"))
+//Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX',
+// а иначе false.
+function checkSpam(str) {
+    
+}
