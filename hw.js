d8bcb8d92fd5f5def4bbe4154e9f629b83d82f5f
Alexandr Pirogov <otbitij@mail.ru>
2023-02-07 11:18:09 +0300

    короче ничего не работает как обычно

--- hw.js
+++ hw.js
@@ -4,7 +4,7 @@
 function ucFirst(str) {
     for (let i = 0; i < str.lenght; i++ ) {
         if (i = 0) {
-            str[0].toUpperCase
+            str[0].toUpperCase()
         }
         str[i] += str[i++]
     }
@@ -14,5 +14,12 @@ console.log(ucFirst("линия"))
 //Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX',
 // а иначе false.
 function checkSpam(str) {
-    
+    str.toLowerCase()
+    if (str.includes("viagra") || str.includes("xxx")) {
+        return true
+    } else {
+        return false
+    }
 }
+let str = "бла бла Viagra бла"
+console.log(checkSpam(str))
