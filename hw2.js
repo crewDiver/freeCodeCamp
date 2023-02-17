66b9ee76f72d71568dfbfd516cf40f34187d64aa
Alexandr Pirogov <otbitij@mail.ru>
2023-02-17 17:02:32 +0300

    знятие

--- hw2.js
+++ hw2.js
@@ -6,44 +6,44 @@ const sums = [
     [1, 3]
 ]
 function max(arr) {
-    arr.reduce((sum, item) => {
-      return sum.push(item.forEach(element => {
-            let sum1 = 0
-            return sum1 += element
-        })) 
+  let arrOfSum = arr.reduce((arrSum, item) => {
+        let sum = 0
+        item.forEach((numb) => {
+            sum += numb
+        })
+        arrSum.push(sum)
+        return arrSum
     },[])
-    sum.sort((a, b) => {
-        return b - a
-    })
-    return sum[0]
+    arrOfSum.sort((a, b) => a - b)
+    return arrOfSum[arrOfSum.length - 1] 
 }
 console.log(max(sums))
 
 function min(arr) {
-    arr.reduce((sum, item) => {
-        return sum.push(item.forEach(element => {
-              let sum1 = 0
-              return sum1 += element
-          })) 
+    let arrOfSum = arr.reduce((arrSum, item) => {
+          let sum = 0
+          item.forEach((numb) => {
+              sum += numb
+          })
+          arrSum.push(sum)
+          return arrSum
       },[])
-      sum.sort((a, b) => {
-          return a - b
-      })
-      return sum[0]
-}
+      arrOfSum.sort((a, b) => a - b)
+      return arrOfSum[0] 
+  }
 console.log(min(sums))
 
 function average(arr) {
-    arr.reduce((sum, item) => {
-        return sum.push(item.forEach(element => {
-              let sum1 = 0
-              return sum1 += element
-          })) 
-      },[])
-//здесь должно быть среднее арифметическое
+   let sum =  arr.reduce((sum, item) => {
+        item.forEach((numb) => {
+            sum += numb
+        })
+        return sum
+    },0)
+    return sum / arr.length
 }
-comsole.log(average(sums))
-//вторая задача
+console.log(average(sums))
+// //вторая задача
 const sums1 = [
     [1, 5, 6],
     [7, 2, 4, 6],
@@ -57,15 +57,45 @@ const sums2 = [
     [1, 6, 1]
 ]
 function comparison(arr1, arr2) {
-    arr1.reduce((oddArr1, item) => {
-        oddArr1.push(item)
-    }[])
+    let sum1 =  arr1.reduce((sum, item) => {
+        item.forEach((numb) => {
+            if (numb %2 == 0) {
+                sum += numb
+            }
+        })
+        return sum 
+    },0)
+    let sum2 =  arr2.reduce((sum, item) => {
+        item.forEach((numb) => {
+            if (numb %2 == 0) {
+                sum += numb
+            }
+        })
+        return sum 
+    },0)
+    if (sum1 > sum2) {
+        return sum1
+    } else {
+        return sum2
+    }
 }
-//третья задача
+console.log(comparison(sums1, sums2))
+// //третья задача
 function getDigitsSum(numb) {
-   let digits = numb.split("")
-   return digits.map((item) => {
-    return item +=
-   }) 
+    let str = numb + ""//делает строкой
+   let digits = str.split("")
+   return digits.reduce((sum, item) => {
+    return sum += +item//делает числом
+},0) 
 }
 console.log(getDigitsSum(302))
+function reverseWords(str) {
+   let arr = str.split(" ")
+   let words = arr.map((item) => { 
+    return item.split("").reverse().join("")
+   })
+   return words.join(" ")
+  }
+  console.log(reverseWords("double  spaces"))
+//   "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
