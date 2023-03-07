b329dbcff6a4e2200e0aa21c5f32bd95b3e5653c
Alexandr Pirogov <otbitij@mail.ru>
2023-03-07 21:45:29 +0300

    третья задача

--- hw2.js
+++ hw2.js
@@ -44,67 +44,67 @@
 // }
 // console.log(average(sums))
 // //вторая задача
-const sums1 = [
-    [1, 5, 6],
-    [7, 2, 4, 6],
-    [7, 10, 1],
-    [1, 3]
-]
-const sums2 = [
-    [2, 4, 11],
-    [1, 2, 5, 6],
-    [3, 10, 1],
-    [1, 6, 1]
-]
-function comparison(arr1, arr2) {
-    let sum1 =  arr1.reduce((sum, item) => {
-        item.forEach((numb) => {
-            if (numb %2 == 0) {
-                sum += numb
-            }
-        })
-        return sum 
-    },0)
-    let sum2 =  arr2.reduce((sum, item) => {
-        item.forEach((numb) => {
-            if (numb %2 == 0) {
-                sum += numb
-            }
-        })
-        return sum 
-    },0)
-    if (sum1 > sum2) {
-        return sum1
-    } else {
-        return sum2
-    }
-}
-console.log(comparison(sums1, sums2))
-// // //третья задача
-// function getDigitsSum(numb) {
-//     let str = numb + ""//делает строкой
-//    let digits = str.split("")
-//    return digits.reduce((sum, item) => {
-//     return sum += +item//делает числом
-// },0) 
-// }
-// console.log(getDigitsSum(302))
-// function reverseWords(str) {
-//    let arr = str.split(" ")
-//    let words = arr.map((item) => { 
-//     return item.split("").reverse().join("")
-//    })
-//    return words.join(" ")
-//   }
-//   console.log(reverseWords("double  spaces"))
-// //   "This is an example!" ==> "sihT si na !elpmaxe"
-// // "double  spaces"      ==> "elbuod  secaps"
-// function century(year) {
-//     let vek = year %100
-//     if (vek == 0) {
-//         return year / 100
+// const sums1 = [
+//     [1, 5, 6],
+//     [7, 2, 4, 6],
+//     [7, 10, 1],
+//     [1, 3]
+// ]
+// const sums2 = [
+//     [2, 4, 11],
+//     [1, 2, 5, 6],
+//     [3, 10, 1],
+//     [1, 6, 1]
+// ]
+// function comparison(arr1, arr2) {
+//     let sum1 =  arr1.reduce((sum, item) => {
+//         item.forEach((numb) => {
+//             if (numb %2 == 0) {
+//                 sum += numb
+//             }
+//         })
+//         return sum 
+//     },0)
+//     let sum2 =  arr2.reduce((sum, item) => {
+//         item.forEach((numb) => {
+//             if (numb %2 == 0) {
+//                 sum += numb
+//             }
+//         })
+//         return sum 
+//     },0)
+//     if (sum1 > sum2) {
+//         return sum1
 //     } else {
-//         return Math.ceil(year / 100)
+//         return sum2
 //     }
-// } 
-// console.log(century(988))
+// }
+// console.log(comparison(sums1, sums2))
+// // //третья задача
+function getDigitsSum(numb) {
+    let str = numb + ""//делает строкой
+   let digits = str.split("")
+   return digits.reduce((sum, item) => {
+    return sum += +item//делает числом
+},0) 
+}
+console.log(getDigitsSum(302))
+function reverseWords(str) {
+   let arr = str.split(" ")
+   let words = arr.map((item) => { 
+    return item.split("").reverse().join("")
+   })
+   return words.join(" ")
+  }
+  console.log(reverseWords("double  spaces"))
+//   "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+function century(year) {
+    let vek = year %100
+    if (vek == 0) {
+        return year / 100
+    } else {
+        return Math.ceil(year / 100)
+    }
+} 
+console.log(century(988))
